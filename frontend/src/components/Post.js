58886b71e9cef00d74970ed9f6d1702549f5002a
Alfredo Ribeiro <me@alfredoribeiro.com>
2018-10-11 10:58:44 -0300

    Popoulate post, upVote, downVote

--- frontend/src/components/Post.js
+++ frontend/src/components/Post.js
@@ -1,12 +1,49 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import * as API from '../ReadableAPI'
 
 class Post extends Component {
+    state = {post: null}
+    componentDidMount(){
+        const { post } = this.props 
+        this.setState({post})
+    }
+
+    upVote = () => {
+        API.votePost(this.state.post.id, 'upVote')
+            .then(post => {
+                this.setState({post})
+            })
+    }
+    downVote = () => {
+        API.votePost(this.state.post.id, 'downVote')
+            .then(post => {
+                this.setState({post})
+            })
+    }
+
+    addComment = () => {
+        console.log('Add Comment Here')
+    }
+
     render(){
-        return (
-            <li>
-                <h2>{this.props.title}</h2>
-            </li>
-        )
+        const { post } = this.state
+        if(post){
+            return (
+                <li>
+                    <h2>{post.title}</h2>
+                    <h3>{post.voteScore} {post.voteScore > 1 || post.voteScore < -1 ? "Points" : "Vote"} </h3>
+                    <button onClick={this.upVote}>+</button>
+                    <button onClick={this.downVote}>-</button>
+                    <div>{post.body}</div>
+                    <h4>Author: {post.author}</h4>
+                    <p>{post.commentCount} {post.commentCount > 1 ? "Comments" : "Comment"}</p>
+                    <button onClick={this.addComment}>ADD COMMENT</button>
+                </li>
+            )
+        }else{
+            return <li>Loading ...</li>
+        }
     }
 }
 
